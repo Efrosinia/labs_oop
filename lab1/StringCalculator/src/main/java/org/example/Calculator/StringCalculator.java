3fd9314b0b95213e551adae5a9419ff17766e0b3
Efrosinia <hfgaf>
2023-11-05 00:59:20 +0100

    Step 2

--- lab1/StringCalculator/src/main/java/org/example/Calculator/StringCalculator.java
+++ lab1/StringCalculator/src/main/java/org/example/Calculator/StringCalculator.java
@@ -5,23 +5,18 @@ import java.util.Objects;
 public class StringCalculator {
     public int add(String numbers) throws IncorrectInput {
         StringBuilder Incorchars= new StringBuilder();
-        if (numbers.isEmpty()) {
+
+        if (numbers.isEmpty() ) {
             return 0;
         }
-        if (numbers.contains(",,")) {
-            throw new IncorrectInput("you cannot enter mathematical expressions: ,, ");
-        }
+
         String[] numberArray = numbers.split(",");
-        if(numberArray.length>2)
-        {
-            throw new IncorrectInput("the number of digits should be no more than two ");
-        }
         int sum = 0;
 
         for (String num : numberArray) {
-            if(num.isEmpty())
+            if (num.isEmpty())
             {
-                throw new IncorrectInput("you cannot enter mathematical expressions: "+ numbers);
+                throw new IncorrectInput("you cannot enter mathematical expression: "+numbers);
             }
             if (!num.matches("-?\\d+") )
             {
@@ -37,7 +32,7 @@ public class StringCalculator {
                 }
                 if (count>1)
                 {
-                    throw new IncorrectInput("you cannot enter mathematical expressions:  "+num);
+                    throw new IncorrectInput("you cannot enter mathematical expression: "+num);
                 }
             }
         }
@@ -53,5 +48,6 @@ public class StringCalculator {
 
         return sum;
     }
+
 
 }
